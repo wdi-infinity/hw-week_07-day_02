9d0218a1d4dd2193c3e31b8df320a681b3f62de4
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-02-26 09:11:52 +0300

    First try

--- mbta.rb
+++ mbta.rb
@@ -1,5 +1,60 @@
 
 # your solution here
-def stops_between_stations
+# hash or array?
+line_stations = {
+red_Line : ["South Station",
+"Park Street",
+"Kendall",
+"Central",
+"Harvard",
+"Porter",
+"Davis",
+"Alewife"],
+#green_Line : ["Government Center",
+#"Park Street",
+#"Boylston",
+#"Arlington",
+#"Copley",
+#"Hynes",
+#"Kenmore"],
+#orange_Line:
+#["North Station",
+#"Haymarket",
+#"Park Street",
+#"State",
+#"Downtown Crossing",
+#"Chinatown",
+#"Back Bay",
+#"Forest Hills"]
+}
 
-end
+#def stops_between_stations(fline,fstation,sline,lstation)
+# datatype of arguments array,string,array,string
+#station_counter_first_line (lines,station) call the function
+#if fline == sline && fstation==lstation
+#puts return value from station_counter_first_line (lines,station)
+#else 
+#station_counter_second_line (lines,station,total of stations from the first line) call the function
+#end
+
+#end
+
+
+#def station_counter_first_line (lines,station)
+    # datatype of arguments array,string
+    #need to loop throw one line and increment counter to calculate the stations
+    #loop need to be breaked if station == "Park Street"
+    #return the number of stations
+    
+
+#end
+
+#def station_counter_second_line (slines,lstation,total)
+    # datatype of arguments array,string
+    # if station == "Park Street"
+    #need to loop throw one line and increment counter to calculate the stations and save it in variable
+    #return number of stations + total
+    
+
+
+#end
